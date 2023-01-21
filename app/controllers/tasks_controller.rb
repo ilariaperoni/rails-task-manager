a6c5023c33dac8ed097df131ec6d1dfe511d9198
Ilaria Peroni <ilaria.peroni13@gmail.com>
2023-01-21 16:25:53 +0000

    Add create view and method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,21 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new # needed to initiate the form_for helper
+  end
+
+  def create
+    puts task_params
+    task = Task.new(task_params)
+    task.save
+    redirect_to task_path(task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
